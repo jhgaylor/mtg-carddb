8018937309b90563e91c24fc420eb376cd25314d
Jake Gaylor <jhgaylor@gmail.com>
2015-08-31 00:20:27 -0600

    update description and packaging

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'mtg:carddb',
   version: '0.0.3',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Meteor package to consume mtg:json and create a mongodb collection representing the data.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jhgaylor/mtg-carddb.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -15,9 +15,9 @@ Package.onUse(function(api) {
   api.use('mongo');
   api.use('underscore');
   api.use('aldeed:collection2@2.5.0');
-  api.use('aldeed:simple-schema');
+  api.use('aldeed:simple-schema@1.0.0');
   api.use('dburles:collection-helpers@1.0.0');
-  api.use('mtg:json');
+  api.use('mtg:json@0.0.0');
   api.addFiles('Cards.js');
   api.addFiles('carddb.js');
   api.export('MTGCard');
